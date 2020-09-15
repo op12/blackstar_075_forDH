c2f19c9a23b9752b5a7e2a5e532680bfea376c2b
wikarous <67613740+wikarous@users.noreply.github.com>
2020-09-15 14:00:30 +0900

    Update interface.py
    DH lqr apply

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -74,14 +74,26 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
       ret.minSteerSpeed = 32 * CV.MPH_TO_MS
+    #제네시스 DH 조향 수정 시작
     elif candidate == CAR.HYUNDAI_GENESIS:
-      ret.lateralTuning.pid.kf = 0.00005
-      ret.mass = 2060. + STD_CARGO_KG
+      ret.mass = 1980. + STD_CARGO_KG
       ret.wheelbase = 3.01
-      ret.steerRatio = 16.5
-      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.16], [0.01]]
-      ret.minSteerSpeed = 60 * CV.KPH_TO_MS
+      ret.steerRatio = 12.0
+      ret.steerActuatorDelay = 0.3
+      ret.steerRateCost = 0.55
+      ret.steerLimitTimer = 2.0
+      
+      ret.lateralTuning.init('lqr')
+      ret.lateralTuning.lqr.scale = 1580.0
+      ret.lateralTuning.lqr.ki = 0.015
+      ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
+      ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
+      ret.lateralTuning.lqr.c = [1., 0.]
+      ret.lateralTuning.lqr.k = [-100., 450.]
+      ret.lateralTuning.lqr.l = [0.22, 0.318]
+      ret.lateralTuning.lqr.dcGain = 0.003
+    #제네시스 DH 조향 수정 종료 
+      
     elif candidate in [CAR.GENESIS_G80]:
       ret.mass = 2200
       ret.wheelbase = 3.15
